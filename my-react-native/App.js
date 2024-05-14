ccf3077d326d7c4de157773c9f139d77fe62a284
Akashpatel2001 <akp18129@gmail.com>
2024-05-15 02:10:35 +0530

    Added text & view elements

--- my-react-native/App.js
+++ my-react-native/App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, Modal } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleButtonClick = () => {
+    setModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.customtext}>Hello World!</Text>
+      <Button title='Click me dude' onPress={handleButtonClick} />
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={handleCloseModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalMessage}>Thanks for clicking this button. Now press close button to close this modal.</Text>
+            <Button title="Close" onPress={handleCloseModal} />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -16,5 +39,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  customtext: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "red",
+    padding: 16
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalMessage: {
+    marginBottom: 20,
+    textAlign: 'center',
   },
 });
